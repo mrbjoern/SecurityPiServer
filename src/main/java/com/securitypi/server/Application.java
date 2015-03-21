146a0c0abc192e92ae6ed864fe2a64baaa4be810
mrbjoern <mrbjoern92@gmail.com>
2015-03-21 11:36:49 +0100

    Implements SSL on server. New port is 8443

--- src/main/java/com/securitypi/server/Application.java
+++ src/main/java/com/securitypi/server/Application.java
@@ -3,15 +3,25 @@ package com.securitypi.server;
 import com.securitypi.server.events.Event;
 import com.securitypi.server.events.EventHandler;
 import com.securitypi.server.events.SystemStartedEvent;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.FileNotFoundException;
 import java.util.Date;
 import java.util.Properties;
 
@@ -22,6 +32,9 @@ import java.util.Properties;
 @Configuration
 @ComponentScan({"com.securitypi"})
 public class Application extends WebMvcConfigurerAdapter {
+
+	@Value("${keystore.file}") private String keystoreFile;
+	@Value("${keystore.pass}") private String keystorePass;
 
     public static void main(String args[]) throws Throwable {
         new TemperatureReadingsHandler();
@@ -40,15 +53,30 @@ public class Application extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties(){
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		Properties server = new Properties();
+	public EmbeddedServletContainerCustomizer containerCustomizer() throws FileNotFoundException {
+		final String absoluteKeystoreFile = ResourceUtils.getFile(keystoreFile).getAbsolutePath();
 
-		server.setProperty("server.tomcat.remote_ip_header", "x-forwarded-for");
-		server.setProperty("spring.thymeleaf.cache", "false");
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer factory) {
+				if(factory instanceof TomcatEmbeddedServletContainerFactory) {
+					TomcatEmbeddedServletContainerFactory containerFactory = (TomcatEmbeddedServletContainerFactory) factory;
+					containerFactory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+						@Override
+						public void customize(Connector connector) {
+							connector.setSecure(true);
+							connector.setScheme("https");
 
-		propertySourcesPlaceholderConfigurer.setProperties(server);
-
-		return propertySourcesPlaceholderConfigurer;
+							Http11NioProtocol proto = (Http11NioProtocol) connector.getProtocolHandler();
+							proto.setSSLEnabled(true);
+							proto.setKeystoreFile(absoluteKeystoreFile);
+							proto.setKeystorePass(keystorePass);
+							proto.setKeystoreType("PKCS12");
+							proto.setKeyAlias("tomcat");
+						}
+					});
+				}
+			}
+		};
 	}
 }
